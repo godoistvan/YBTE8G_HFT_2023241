7e8c5d1e9cf2189652894bcd223e128ca9436558
godoistvan <godopisti@gmail.com>
2023-10-26 13:40:08 +0200

    Repository class is now functional after the changes and implement an empty version of all the CRUD methods

--- YBTE8G_HFT_2023241.Repository/Repositories/Repository.cs
+++ YBTE8G_HFT_2023241.Repository/Repositories/Repository.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YBTE8G_HFT_2023241.Models;
 using YBTE8G_HFT_2023241.Repository.Interfaces;
 
 namespace YBTE8G_HFT_2023241.Repository.Repositories
 {
-    internal class Repository<T> : IRepository<T> where T : class
+    internal class Repository<T> : IRepository<T> where T : Entity
     {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
