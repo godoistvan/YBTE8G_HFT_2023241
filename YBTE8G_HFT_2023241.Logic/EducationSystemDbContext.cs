8596d1bcacc69d0af85da299e06cc262596422e7
godoistvan <godopisti@gmail.com>
2023-10-26 13:46:33 +0200

    Created the appropiate dbcontext class and installed entityframework nuget package across the project

---
+++ YBTE8G_HFT_2023241.Logic/EducationSystemDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YBTE8G_HFT_2023241.Models;
+
+namespace YBTE8G_HFT_2023241.Logic
+{
+    public partial  class EducationSystemDbContext : DbContext  
+    {
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Class> Classes { get; set; }
+    }
+}
