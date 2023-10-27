5b7fb4f343cbf3a627d0bab7240d30b3d75c341d
godoistvan <godopisti@gmail.com>
2023-10-27 14:47:08 +0200

    Altered classID-s for the students

--- YBTE8G_HFT_2023241.Logic/EducationSystemDbContext.cs
+++ YBTE8G_HFT_2023241.Logic/EducationSystemDbContext.cs
@@ -54,26 +54,26 @@ namespace YBTE8G_HFT_2023241.Logic
 
             List<Student> students = new List<Student>
 {
-    new Student { Id = 1, Name = "Kovács István", Email = "kovacs.istvan@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 2, Name = "Nagy János", Email = "nagy.janos@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 3, Name = "Tóth Mária", Email = "toth.maria@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 4, Name = "Varga Katalin", Email = "varga.katalin@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 5, Name = "Horváth Péter", Email = "horvath.peter@gmail.com", PhoneNumber = "+36301122334", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 6, Name = "Kiss Emese", Email = "kiss.emese@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 7, Name = "Farkas Zoltán", Email = "farkas.zoltan@gmail.com", PhoneNumber = "+36308901234", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 8, Name = "Szabó Anna", Email = "szabo.anna@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 9, Name = "Molnár Gábor", Email = "molnar.gabor@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 10, Name = "Balogh Károly", Email = "balogh.karoly@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 4, ClassID = 4 },
-    new Student { Id = 11, Name = "Takács Andrea", Email = "takacs.andrea@gmail.com", PhoneNumber = "+36308901234", SemestersIn = 5, ClassID = 5 },
-    new Student { Id = 12, Name = "Mészáros Gergő", Email = "meszaros.gergo@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 5, ClassID = 5 },
-    new Student { Id = 13, Name = "Papp Krisztina", Email = "papp.krisztina@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 5, ClassID = 5 },
-    new Student { Id = 14, Name = "Király Bence", Email = "kiraly.bence@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 5, ClassID = 5 },
-    new Student { Id = 15, Name = "Fehér Dóra", Email = "feher.dora@gmail.com", PhoneNumber = "+36301122334", SemestersIn = 5, ClassID = 5 },
-    new Student { Id = 16, Name = "Bodor László", Email = "bodor.laszlo@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 5, ClassID = 5 },
-    new Student { Id = 17, Name = "Csernai Eszter", Email = "csernai.eszter@gmail.com", PhoneNumber = "+36308901234", SemestersIn = 5, ClassID = 5 },
-    new Student { Id = 18, Name = "Fodor Gábor", Email = "fodor.gabor@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 6, ClassID = 6 },
-    new Student { Id = 19, Name = "Bencze Enikő", Email = "bencze.eniko@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 6, ClassID = 6 },
-    new Student { Id = 20, Name = "Lengyel Zsolt", Email = "lengyel.zsolt@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 6, ClassID = 6 }
+    new Student { Id = 1, Name = "Kovács István", Email = "kovacs.istvan@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 4, ClassID = 7 },
+    new Student { Id = 2, Name = "Nagy János", Email = "nagy.janos@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 4, ClassID = 7 },
+    new Student { Id = 3, Name = "Tóth Mária", Email = "toth.maria@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 4, ClassID = 8 },
+    new Student { Id = 4, Name = "Varga Katalin", Email = "varga.katalin@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 4, ClassID = 8 },
+    new Student { Id = 5, Name = "Horváth Péter", Email = "horvath.peter@gmail.com", PhoneNumber = "+36301122334", SemestersIn = 4, ClassID = 7 },
+    new Student { Id = 6, Name = "Kiss Emese", Email = "kiss.emese@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 4, ClassID = 7 },
+    new Student { Id = 7, Name = "Farkas Zoltán", Email = "farkas.zoltan@gmail.com", PhoneNumber = "+36308901234", SemestersIn = 4, ClassID = 7 },
+    new Student { Id = 8, Name = "Szabó Anna", Email = "szabo.anna@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 4, ClassID = 7 },
+    new Student { Id = 9, Name = "Molnár Gábor", Email = "molnar.gabor@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 4, ClassID = 8 },
+    new Student { Id = 10, Name = "Balogh Károly", Email = "balogh.karoly@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 4, ClassID = 8 },
+    new Student { Id = 11, Name = "Takács Andrea", Email = "takacs.andrea@gmail.com", PhoneNumber = "+36308901234", SemestersIn = 5, ClassID = 10 },
+    new Student { Id = 12, Name = "Mészáros Gergő", Email = "meszaros.gergo@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 5, ClassID = 10 },
+    new Student { Id = 13, Name = "Papp Krisztina", Email = "papp.krisztina@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 5, ClassID = 9 },
+    new Student { Id = 14, Name = "Király Bence", Email = "kiraly.bence@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 5, ClassID = 9 },
+    new Student { Id = 15, Name = "Fehér Dóra", Email = "feher.dora@gmail.com", PhoneNumber = "+36301122334", SemestersIn = 5, ClassID = 10 },
+    new Student { Id = 16, Name = "Bodor László", Email = "bodor.laszlo@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 5, ClassID = 10 },
+    new Student { Id = 17, Name = "Csernai Eszter", Email = "csernai.eszter@gmail.com", PhoneNumber = "+36308901234", SemestersIn = 5, ClassID = 10 },
+    new Student { Id = 18, Name = "Fodor Gábor", Email = "fodor.gabor@gmail.com", PhoneNumber = "+36301234567", SemestersIn = 6, ClassID = 12  },
+    new Student { Id = 19, Name = "Bencze Enikő", Email = "bencze.eniko@gmail.com", PhoneNumber = "+36304567890", SemestersIn = 6, ClassID = 12},
+    new Student { Id = 20, Name = "Lengyel Zsolt", Email = "lengyel.zsolt@gmail.com", PhoneNumber = "+36307890123", SemestersIn = 6, ClassID = 11 }
 };
             List<Course> courses = new List<Course>()
 {
