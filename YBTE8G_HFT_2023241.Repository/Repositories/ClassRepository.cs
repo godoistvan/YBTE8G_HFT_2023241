9a8ec6388cf64d9c5c6995c8663353b7f71307c6
godoistvan <godopisti@gmail.com>
2023-11-02 18:15:43 +0100

    Implemented ClassRepository

--- YBTE8G_HFT_2023241.Repository/Repositories/ClassRepository.cs
+++ YBTE8G_HFT_2023241.Repository/Repositories/ClassRepository.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YBTE8G_HFT_2023241.Logic;
+using YBTE8G_HFT_2023241.Models;
+using YBTE8G_HFT_2023241.Repository.Interfaces;
 
 namespace YBTE8G_HFT_2023241.Repository.Repositories
 {
-    internal class ClassRepository
+    public class ClassRepository : Repository<Class>, IClassRepository
     {
+        public ClassRepository(EducationSystemDbContext ctx) : base(ctx)
+        {
+
+        }
+        public void UpdateSemester(int id,int newSemester)
+        {
+            Class old = Read(id);
+            old.Semester = newSemester;
+            ctx.SaveChanges();
+        }
     }
 }
