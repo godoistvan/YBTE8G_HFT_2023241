5425b92153f2ea4b89f376ed860370cb2cafc81d
godoistvan <godopisti@gmail.com>
2023-11-02 18:29:53 +0100

    Implemented basic ICourseLogic interface

--- YBTE8G_HFT_2023241.Logic/Interfaces/ICourseLogic.cs
+++ YBTE8G_HFT_2023241.Logic/Interfaces/ICourseLogic.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YBTE8G_HFT_2023241.Models;
 
 namespace YBTE8G_HFT_2023241.Logic.Interfaces
 {
-    internal interface ICourseLogic
+    public interface ICourseLogic
     {
+        void Create(Course course);
+        void Delete(int id);
+        IEnumerable<Course> ReadAll();
+        Course Read(int id);
+        void Update(Course course);
     }
 }
