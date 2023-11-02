d4b72223cc65452928ef9cadb9a722f4a0632353
godoistvan <godopisti@gmail.com>
2023-11-02 18:31:03 +0100

    Implemented basic IClassLogic interface

--- YBTE8G_HFT_2023241.Logic/Interfaces/IClassLogic.cs
+++ YBTE8G_HFT_2023241.Logic/Interfaces/IClassLogic.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YBTE8G_HFT_2023241.Models;
 
 namespace YBTE8G_HFT_2023241.Logic.Interfaces
 {
     internal interface IClassLogic
     {
+        void Create(Class classcreate);
+        void Delete(int id);
+        IEnumerable<Class> ReadAll();
+        Class Read(int id);
+        void Update(Course classupdate);
     }
 }
