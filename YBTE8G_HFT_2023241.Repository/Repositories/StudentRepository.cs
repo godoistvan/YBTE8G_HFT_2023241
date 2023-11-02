10c59508418b0ac81c6348fa47543fff81479f2e
godoistvan <godopisti@gmail.com>
2023-11-02 18:20:05 +0100

    Implemented StudentRepository

--- YBTE8G_HFT_2023241.Repository/Repositories/StudentRepository.cs
+++ YBTE8G_HFT_2023241.Repository/Repositories/StudentRepository.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YBTE8G_HFT_2023241.Logic;
+using YBTE8G_HFT_2023241.Models;
+using YBTE8G_HFT_2023241.Repository.Interfaces;
 
 namespace YBTE8G_HFT_2023241.Repository.Repositories
 {
-    internal class StudentRepository
+    public class StudentRepository : Repository<Student>, IStudentRepository
     {
+        public StudentRepository(EducationSystemDbContext ctx) : base(ctx)
+        {
+
+        }
+        public void UpdateSemester(int id,int newSemesterIn)
+        {
+            Student old = Read(id);
+            old.SemestersIn = newSemesterIn;
+            ctx.SaveChanges();
+        }
     }
 }
