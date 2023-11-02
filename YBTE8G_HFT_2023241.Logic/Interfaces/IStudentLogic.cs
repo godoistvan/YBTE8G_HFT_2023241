eb9f0ced7d95d5606182fa8979b9a29b3ea106d1
godoistvan <godopisti@gmail.com>
2023-11-02 18:31:56 +0100

    Implemented basic IStudenLogic interface

--- YBTE8G_HFT_2023241.Logic/Interfaces/IStudentLogic.cs
+++ YBTE8G_HFT_2023241.Logic/Interfaces/IStudentLogic.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YBTE8G_HFT_2023241.Models;
 
 namespace YBTE8G_HFT_2023241.Logic.Interfaces
 {
-    internal interface IStudentLogic
+    public interface IStudentLogic
     {
+        void Create(Student student);
+        void Delete(int id);
+        IEnumerable<Student> ReadAll();
+        Student Read(int id);
+        void Update(Student student);
     }
 }
