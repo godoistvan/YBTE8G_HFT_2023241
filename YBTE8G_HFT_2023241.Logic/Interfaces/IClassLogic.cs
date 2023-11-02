1828824f1a023e33e290c57d0f9bfe13547d5205
godoistvan <godopisti@gmail.com>
2023-11-02 18:24:38 +0100

    Created the necessary interfaces for logic

---
+++ YBTE8G_HFT_2023241.Logic/Interfaces/IClassLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YBTE8G_HFT_2023241.Logic.Interfaces
+{
+    internal interface IClassLogic
+    {
+    }
+}
