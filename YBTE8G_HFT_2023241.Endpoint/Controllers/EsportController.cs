cfe4c2105c2c10d16e60e0061849e1fdc8f410f2
godoistvan <godopisti@gmail.com>
2023-11-22 13:12:54 +0100

    Added EsportController to Endpoint

---
+++ YBTE8G_HFT_2023241.Endpoint/Controllers/EsportController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YBTE8G_HFT_2023241.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EsportController : ControllerBase
+    {
+        // GET: api/<EsportController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EsportController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EsportController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EsportController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EsportController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
