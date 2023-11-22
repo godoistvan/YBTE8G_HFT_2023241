15b32b53679ff20527f7c0bb0c0fd0b3b7b19996
godoistvan <godopisti@gmail.com>
2023-11-22 13:26:41 +0100

    Implemented CRUD methods to EsportController

--- YBTE8G_HFT_2023241.Endpoint/Controllers/EsportController.cs
+++ YBTE8G_HFT_2023241.Endpoint/Controllers/EsportController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using YBTE8G_HFT_2023241.Logic.Interfaces;
+using YBTE8G_HFT_2023241.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,35 +13,43 @@ namespace YBTE8G_HFT_2023241.Endpoint.Controllers
     {
         IGameLogic logic;
         // GET: api/<EsportController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        
+        public EsportController(IGameLogic logic)
         {
-            return new string[] { "value1", "value2" };
+            this.logic = logic;
+        }
+        [HttpGet]
+        public IEnumerable<Game> ReadAll()
+        {
+            return this.logic.ReadAll();
         }
 
         // GET api/<EsportController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Game Read(int id)
         {
-            return "value";
+            return this.logic.Read(id);
         }
 
         // POST api/<EsportController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Create([FromBody] Game value)
         {
+            this.logic.Create(value);
         }
 
         // PUT api/<EsportController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Update([FromBody] Game value)
         {
+            this.logic.Update(value);
         }
 
         // DELETE api/<EsportController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            this.logic.Delete(id);
         }
     }
 }
